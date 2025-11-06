3a02f578dfda851e3566f347c0cc8300b0d93883
Pablo Di Taranto <103513283+PDiTaranto@users.noreply.github.com>
2025-11-06 12:07:05 -0300

    AI Init

---
+++ Source/Aura/Public/AI/AuraAIController.h
@@ -0,0 +1,27 @@
+// Copyright Pablo Di Taranto
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AuraAIController.generated.h"
+
+class UBehaviorTreeComponent;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+
+	AAuraAIController();
+
+protected:
+
+
+	UPROPERTY()
+	TObjectPtr<UBehaviorTreeComponent> BehaviorTreeComponent;
+};
--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -46,7 +46,10 @@ protected:
 	void OnEndOverlap(AActor* TargetActor);
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Applied Effects")
-	bool bDestroyOnEffectRemoval = false;
+	bool bDestroyOnEffectApplication = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Applied Effects")
+	bool bApplyEffectsToEnemies = false;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
